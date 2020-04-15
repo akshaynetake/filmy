cb7310c29c413e2e137566a92184d911427d1b5b
Akshay Netake <akshay.netake@gmail.com>
2020-04-16 01:46:32 +0530

    added the basic api service

---
+++ src/app/services/api-service.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnInit } from '@angular/core';
+
+import { HttpClient, HttpParams, HttpHeaders,HttpUrlEncodingCodec, HttpEventType } from "@angular/common/http";
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+}
