88b7f6476ac18c19f6a1a592fc05b7eec88fc5c4
sanket <sanket.andhale1@gmail.com>
2020-04-19 15:54:47 +0530

    social login using facebook and google added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
 
+import { SocialLoginModule, AuthServiceConfig,GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+
+//auth service social login config
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+    [
+      {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider("2893786697368668")
+      },
+      {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider("288191163185-lkfvsnot08j0a79ciqprhj5pdeh8b31v.apps.googleusercontent.com")
+      }
+    ]
+  );
+  return config;
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,9 +32,15 @@ import { LoginSignupComponent } from './login-signup/login-signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: getAuthServiceConfigs
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
@@ -7,23 +8,113 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginSignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private socialAuthService: AuthService) { }
 
-  login;
-  signup;
+  isLoginVisible;
+  isSignupVisible;
+  currentUser = {};
+  isDataLoading:any = false;
+  socialSignInUserData;
+
 
   ngOnInit() {
-    this.login = true;
+    this.isLoginVisible = true;
   }
 
   visibleSignUp(){
-    this.signup = true
-    this.login = false
+    this.isSignupVisible = true
+    this.isLoginVisible = false
   }
 
   visibleLogin(){
-    this.signup = false
-    this.login = true
+    this.isSignupVisible = false
+    this.isLoginVisible = true
   }
+
+
+
+  public socialSignIn(socialPlatform : string) {
+    console.log('socialPlatform: ', socialPlatform);
+
+
+
+      this.isDataLoading = false;
+      let socialPlatformProvider;
+          if(socialPlatform == "facebook"){
+            socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+          }else if(socialPlatform == "google"){
+            socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+          } 
+          var thisVar = this;
+          this.socialAuthService.signIn(socialPlatformProvider).then(
+            (userData) => {
+              console.log('userData: ', userData);
+              
+              // // console.table(userData);
+              
+              // var body = {
+              //   email : userData.email,
+              //   id : userData.id,
+              //   name : userData.name,
+              //   provider : userData.provider,
+              //   token : userData.token,
+              //   image : userData.image
+              // }
+              // if(socialPlatform == "google"){
+              //   body['token'] = userData.idToken;
+              // }
+              // var toogleAlert;
+              // if(socialPlatform == "facebook" && body.email =='' || body.email == undefined){
+              //   thisVar.userService.showAlert('We did not receive an email address from Facebook, in order for you to sign up and use the unsyned.us web portal we will need an email address. Please attempt to connect with Facebook again or try connecting with a Google account.', 'failed');
+              //   this.socialAuthService.signOut().then( () => {
+              //   });
+              // } 
+              // else {
+              //   thisVar.userService.showToogleAlert('Processing', 'process','init',(res)=>{
+              //     toogleAlert = res;
+              //   });
+                
+              //   thisVar.api.socialLogin(body).subscribe(
+              //     data => {
+                    
+              //       // console.table(data);
+                    
+              //       if (data['code'] == 200) {
+              //         localStorage.setItem('wyreuser', JSON.stringify(data['data']));
+              //         toogleAlert.componentInstance.subtype = 'done';
+              //         toogleAlert.componentInstance.title = 'Done';
+              //         setTimeout(function () {
+              //           toogleAlert.close();
+              //         }, 2000);
+              //         thisVar.router.navigate(['/dashboard']);
+              //       } else {
+              //         this.socialAuthService.signOut().then( () => {
+              //         });
+              //         toogleAlert.componentInstance.subtype = 'failed';
+              //         toogleAlert.componentInstance.title = data['msg'];
+              //         setTimeout(function () {
+              //           toogleAlert.close();
+              //         }, 3000);
+              //       }
+              //     },
+              //     error => {
+              //       this.socialAuthService.signOut().then( () => {
+              //       });
+              //       toogleAlert.componentInstance.subtype = 'failed';
+              //       toogleAlert.componentInstance.title = 'Failed to reach server';
+              //       setTimeout(function () {
+              //           toogleAlert.close();
+              //       }, 3000);
+              //       return error;
+              //     }
+              //   );
+              // }   
+            }
+          );
+
+    
+  }
+
+
 
 }
