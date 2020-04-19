3a63da568c3fc9d9c9ce7595041a5bcbd98dade5
sanket167 <sanket@local>
2020-04-20 00:59:13 +0530

    social sign in removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
 
-import { SocialLoginModule, AuthServiceConfig,GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+// import { SocialLoginModule, AuthServiceConfig,GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
 
 //auth service social login config
-export function getAuthServiceConfigs() {
-  let config = new AuthServiceConfig(
-    [
-      {
-        id: FacebookLoginProvider.PROVIDER_ID,
-        provider: new FacebookLoginProvider("2893786697368668")
-      },
-      {
-        id: GoogleLoginProvider.PROVIDER_ID,
-        provider: new GoogleLoginProvider("288191163185-rh55jk34kplkk9e2v04hu5mae57t751s.apps.googleusercontent.com")
-      }
-    ]
-  );
-  return config;
-}
+// export function getAuthServiceConfigs() {
+//   let config = new AuthServiceConfig(
+//     [
+//       {
+//         id: FacebookLoginProvider.PROVIDER_ID,
+//         provider: new FacebookLoginProvider("2893786697368668")
+//       },
+//       {
+//         id: GoogleLoginProvider.PROVIDER_ID,
+//         provider: new GoogleLoginProvider("288191163185-rh55jk34kplkk9e2v04hu5mae57t751s.apps.googleusercontent.com")
+//       }
+//     ]
+//   );
+//   return config;
+// }
 
 @NgModule({
   declarations: [
@@ -33,13 +33,13 @@ export function getAuthServiceConfigs() {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SocialLoginModule
+    // SocialLoginModule
   ],
   providers: [
-    {
-      provide: AuthServiceConfig,
-      useFactory: getAuthServiceConfigs
-    }
+    // {
+    //   provide: AuthServiceConfig,
+    //   useFactory: getAuthServiceConfigs
+    // }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "angularx-social-login";
-import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+// import { AuthService } from "angularx-social-login";
+// import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
@@ -8,7 +8,7 @@ import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-logi
 })
 export class LoginSignupComponent implements OnInit {
 
-  constructor(private socialAuthService: AuthService) { }
+  constructor() { }
 
   isLoginVisible;
   isSignupVisible;
@@ -40,77 +40,77 @@ export class LoginSignupComponent implements OnInit {
 
       this.isDataLoading = false;
       let socialPlatformProvider;
-          if(socialPlatform == "facebook"){
-            socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-          }else if(socialPlatform == "google"){
-            socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-          } 
+          // if(socialPlatform == "facebook"){
+          //   socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+          // }else if(socialPlatform == "google"){
+          //   socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+          // } 
           var thisVar = this;
-          this.socialAuthService.signIn(socialPlatformProvider).then(
-            (userData) => {
-              console.log('userData: ', userData);
+          // this.socialAuthService.signIn(socialPlatformProvider).then(
+          //   (userData) => {
+          //     console.log('userData: ', userData);
               
-              // // console.table(userData);
+          //     // // console.table(userData);
               
-              // var body = {
-              //   email : userData.email,
-              //   id : userData.id,
-              //   name : userData.name,
-              //   provider : userData.provider,
-              //   token : userData.token,
-              //   image : userData.image
-              // }
-              // if(socialPlatform == "google"){
-              //   body['token'] = userData.idToken;
-              // }
-              // var toogleAlert;
-              // if(socialPlatform == "facebook" && body.email =='' || body.email == undefined){
-              //   thisVar.userService.showAlert('We did not receive an email address from Facebook, in order for you to sign up and use the unsyned.us web portal we will need an email address. Please attempt to connect with Facebook again or try connecting with a Google account.', 'failed');
-              //   this.socialAuthService.signOut().then( () => {
-              //   });
-              // } 
-              // else {
-              //   thisVar.userService.showToogleAlert('Processing', 'process','init',(res)=>{
-              //     toogleAlert = res;
-              //   });
+          //     // var body = {
+          //     //   email : userData.email,
+          //     //   id : userData.id,
+          //     //   name : userData.name,
+          //     //   provider : userData.provider,
+          //     //   token : userData.token,
+          //     //   image : userData.image
+          //     // }
+          //     // if(socialPlatform == "google"){
+          //     //   body['token'] = userData.idToken;
+          //     // }
+          //     // var toogleAlert;
+          //     // if(socialPlatform == "facebook" && body.email =='' || body.email == undefined){
+          //     //   thisVar.userService.showAlert('We did not receive an email address from Facebook, in order for you to sign up and use the unsyned.us web portal we will need an email address. Please attempt to connect with Facebook again or try connecting with a Google account.', 'failed');
+          //     //   this.socialAuthService.signOut().then( () => {
+          //     //   });
+          //     // } 
+          //     // else {
+          //     //   thisVar.userService.showToogleAlert('Processing', 'process','init',(res)=>{
+          //     //     toogleAlert = res;
+          //     //   });
                 
-              //   thisVar.api.socialLogin(body).subscribe(
-              //     data => {
+          //     //   thisVar.api.socialLogin(body).subscribe(
+          //     //     data => {
                     
-              //       // console.table(data);
+          //     //       // console.table(data);
                     
-              //       if (data['code'] == 200) {
-              //         localStorage.setItem('wyreuser', JSON.stringify(data['data']));
-              //         toogleAlert.componentInstance.subtype = 'done';
-              //         toogleAlert.componentInstance.title = 'Done';
-              //         setTimeout(function () {
-              //           toogleAlert.close();
-              //         }, 2000);
-              //         thisVar.router.navigate(['/dashboard']);
-              //       } else {
-              //         this.socialAuthService.signOut().then( () => {
-              //         });
-              //         toogleAlert.componentInstance.subtype = 'failed';
-              //         toogleAlert.componentInstance.title = data['msg'];
-              //         setTimeout(function () {
-              //           toogleAlert.close();
-              //         }, 3000);
-              //       }
-              //     },
-              //     error => {
-              //       this.socialAuthService.signOut().then( () => {
-              //       });
-              //       toogleAlert.componentInstance.subtype = 'failed';
-              //       toogleAlert.componentInstance.title = 'Failed to reach server';
-              //       setTimeout(function () {
-              //           toogleAlert.close();
-              //       }, 3000);
-              //       return error;
-              //     }
-              //   );
-              // }   
-            }
-          );
+          //     //       if (data['code'] == 200) {
+          //     //         localStorage.setItem('wyreuser', JSON.stringify(data['data']));
+          //     //         toogleAlert.componentInstance.subtype = 'done';
+          //     //         toogleAlert.componentInstance.title = 'Done';
+          //     //         setTimeout(function () {
+          //     //           toogleAlert.close();
+          //     //         }, 2000);
+          //     //         thisVar.router.navigate(['/dashboard']);
+          //     //       } else {
+          //     //         this.socialAuthService.signOut().then( () => {
+          //     //         });
+          //     //         toogleAlert.componentInstance.subtype = 'failed';
+          //     //         toogleAlert.componentInstance.title = data['msg'];
+          //     //         setTimeout(function () {
+          //     //           toogleAlert.close();
+          //     //         }, 3000);
+          //     //       }
+          //     //     },
+          //     //     error => {
+          //     //       this.socialAuthService.signOut().then( () => {
+          //     //       });
+          //     //       toogleAlert.componentInstance.subtype = 'failed';
+          //     //       toogleAlert.componentInstance.title = 'Failed to reach server';
+          //     //       setTimeout(function () {
+          //     //           toogleAlert.close();
+          //     //       }, 3000);
+          //     //       return error;
+          //     //     }
+          //     //   );
+          //     // }   
+          //   }
+          // );
 
     
   }
