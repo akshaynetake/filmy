814e4b318e900ab3b8372c395f26d8f03f8101b8
Prathamesh <prathamesh.netake@gmail.com>
2020-04-22 02:39:48 +0530

    complated the mail dashboard carosal

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,15 +13,6 @@ export class DashboardComponent implements OnInit {
     {img: "http://placehold.it/350x150/111111"},
     {img: "http://placehold.it/350x150/333333"},
     {img: "http://placehold.it/350x150/666666"},
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
-    {img: "http://placehold.it/350x150/000000"},
-    {img: "http://placehold.it/350x150/111111"},
-    {img: "http://placehold.it/350x150/333333"},
   ];
   slideConfig = {
     "slidesToShow": 3,
